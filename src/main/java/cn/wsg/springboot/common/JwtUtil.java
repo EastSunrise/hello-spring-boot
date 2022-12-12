f743d98d6f2bca72dbaf9e7f4b7f919f82ad1fe3
Kingen <wsg787@126.com>
2022-12-12 22:27:53 +0800

    jwt verify

--- src/main/java/cn/wsg/springboot/common/JwtUtil.java
+++ src/main/java/cn/wsg/springboot/common/JwtUtil.java
@@ -3,13 +3,16 @@ package cn.wsg.springboot.common;
 import cn.wsg.springboot.pojo.LoginUser;
 import cn.wsg.springboot.pojo.SysUser;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Verification;
 
 import java.time.Instant;
 
 public class JwtUtil {
 
+    private static final String SECRET = "wsg";
     private static final long SECONDS_OF_WEEK = 7 * 24 * 3600;
 
     public static String createToken(SysUser user, boolean admin) {
@@ -17,7 +20,7 @@ public class JwtUtil {
                 .withClaim("userId", user.getUserId())
                 .withClaim("admin", admin)
                 .withExpiresAt(Instant.now().plusSeconds(SECONDS_OF_WEEK))
-                .sign(Algorithm.HMAC256(user.getPassword()));
+                .sign(Algorithm.HMAC256(SECRET));
 
     }
 
@@ -29,5 +32,14 @@ public class JwtUtil {
         Integer userId = decode.getClaim("userId").asInt();
         Boolean admin = decode.getClaim("admin").asBoolean();
         return new LoginUser(userId, admin);
+    }
+
+    public static void main(String[] args) {
+        SysUser user = SysUser.builder().userId(12).build();
+        String token = createToken(user, true);
+        Verification verification = JWT.require(Algorithm.HMAC256(SECRET));
+        JWTVerifier verifier = verification.build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        System.out.println(decodedJWT);
     }
 }
