c8a7cf1d1bf3768e27e478db74d7a390d434b885
Kingen <wsg787@126.com>
2022-12-19 15:00:24 +0800

    bugfix

--- src/main/java/cn/wsg/springboot/controller/CloudController.java
+++ src/main/java/cn/wsg/springboot/controller/CloudController.java
@@ -5,10 +5,12 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,23 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cloud/api")
 public class CloudController {
 
-    @PostMapping(value = "/root", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> root(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+    @PostMapping(value = "/root", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<?> root(@RequestParam Map<String, String> params, HttpServletRequest request,
+        HttpServletResponse response) throws ServletException, IOException {
         // todo check params
-        Map<String, String[]> params = request.getParameterMap();
-        String[] api = params.get("api");
-        if (api == null || api.length != 1) {
+        String api = params.get("api");
+        if (StringUtils.isBlank(api)) {
             return ResponseEntity.badRequest().build();
         }
-        if ("test".equals(api[0])) {
+        if ("test".equals(api)) {
             request.getRequestDispatcher("/cloud/api/test").forward(request, response);
         }
         return ResponseEntity.notFound().build();
     }
 
     @PostMapping("/test")
-    public ResponseEntity<?> test() {
-        return ResponseEntity.ok("This is test of api");
+    public ResponseEntity<?> test(@RequestParam Map<String, String> params) {
+        return ResponseEntity.ok("This is test of " + params.get("name"));
     }
 }
